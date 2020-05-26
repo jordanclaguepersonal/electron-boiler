89c73c782b4bb3a4d0713cc0237ebb86cb248400
Jordan Clague <jordaneuanclague+code@gmail.com>
2020-05-26 12:03:26 +0100

    Adding SCSS compilation for both component and global styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './App.scss'
 
 const App = () => {
-  return <h1>Hello, Electron Boilerplate!</h1>
+  return <h1 className={styles.primary}>Hello, Electron Boilerplate!</h1>
 }
 
 export default App
